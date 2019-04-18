332b719170ae2cfc8b89c86a2f5bd152bee893d8
Joshua Simpson <jsimpson@financeit.io>
2019-04-18 16:37:43 -0400

    Joshu is the best

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 require "pry-byebug"
 
-amount = ARGV[0].to_i
+amount = ARGV[0].to_f
 amort = 12
 term = 12
 
@@ -20,3 +20,6 @@ denomenator = (1 + r) ** n - 1
 result = P * (numerator / denomenator)
 
 puts result
+puts "Term (number of payments)" + term
+puts "Amort: " + amort
+puts "Principal amount: " + amount
