f0d5b4653c79afab9e83e959488b50d3012adf87
Leo Jweda <ljweda@financeit.io>
2019-04-18 17:08:46 -0400

    Payment requencies.

--- calculator.rb
+++ calculator.rb
@@ -1,19 +1,24 @@
 require "pry-byebug"
 
 amount = ARGV[0].to_f
-amort = 12
-term = 12
+frequency_name = ARGV[1]
+amort = 1
+term = 1
 
 interest = 0.10
 freq = 'm'
 
+frequencies = {
+    monthly: 12,
+    weekly: 52
+}
 
 # TODO: weekly
-payments_per_year = 12
+payments_per_year = frequencies[frequency_name.to_sym]
 
 r = interest / payments_per_year
 P = amount
-n = amort
+n = amort * payments_per_year
 
 numerator = r * (1 + r) ** n
 denomenator = (1 + r) ** n - 1
@@ -23,8 +28,8 @@ total_cost = result * payments_per_year
 total_interest = total_cost - P
 
 puts "Mortgage payment: #{result}"
-puts "Term (number of payments): #{term}"
-puts "Amort: #{amort}"
+puts "Term (number of payments): #{n}"
+puts "Amort: #{n}"
 puts "Principal amount: #{amount}"
 puts "Interest payments: #{total_interest}"
 puts "Total cost: #{total_cost}"
