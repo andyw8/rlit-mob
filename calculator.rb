0938352968fe4814e43ab8111b6c7ac295fa755a
Ruby Wang <rwang@financeit.io>
2019-04-18 16:45:23 -0400

    Yay

--- calculator.rb
+++ calculator.rb
@@ -19,7 +19,12 @@ numerator = r * (1 + r) ** n
 denomenator = (1 + r) ** n - 1
 result = P * (numerator / denomenator)
 
-puts result
-puts "Term (number of payments)" + term
-puts "Amort: " + amort
-puts "Principal amount: " + amount
+total_cost = result * payments_per_year
+total_interest = total_cost - P
+
+puts "Mortgage payment: #{result}"
+puts "Term (number of payments): #{term}"
+puts "Amort: #{amort}"
+puts "Principal amount: #{amount}"
+puts "Interest payments: #{total_interest}"
+puts "Total cost: #{total_cost}"
