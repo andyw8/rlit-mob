bc3ae66d8f6ea43e8ed23685ef7e9664570db40a
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2019-04-18 16:24:44 -0400

    adding calc

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,3 @@
-
 amort = 12
 term = 12
 
@@ -11,8 +10,8 @@ r = interest / 12
 P = 100_000
 n = amort
 
-numerator = r * (1 +r)**n
-denomenator = (1 + r)**n - 1
+numerator = r * (1 + r) ** n
+denomenator = (1 + r) ** n - 1
 result = P * (numerator / denomenator)
 
 puts result
